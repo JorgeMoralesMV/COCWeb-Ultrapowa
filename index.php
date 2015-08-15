bdd4ea8b50833a514a5be16bf6fc21a46fbffe02
JorgeMoralesMV <gemorales.va@gmail.com>
2015-08-15 16:21:53 -0500

    New base code TwistedNL
    third version of the index with reference to the new source code
    implementations of TwistedNL

--- index.php
+++ index.php
@@ -1,34 +1,56 @@
-<?php
-include "config.php";
-?>
-<table width='550' border='1' align='center' cellpadding='1' cellspacing='1'>
+<table width='676' border='1' align='center' cellpadding='1' cellspacing='1'>
     <tr>
-      <td width='53' align='center' valign='top' bgcolor='#999999'><strong>ID</strong></td>
-      <td width='426' align='center' valign='top' bgcolor='#999999'><strong>Nombre</strong></td>
-      <td width='321' align='center' valign='top' bgcolor='#999999'><strong>Ayuntamiento</strong></td>
-	  <td width='321' align='center' valign='top' bgcolor='#999999'><strong>Ultima conexión</strong></td>
-	  <td width='321' align='center' valign='top' bgcolor='#999999'><strong>Perfil</strong></td>
+      <td width='106' align='center' valign='top' bgcolor='#999999'><strong>Player ID</strong></td>
+      <td width='96' align='center' valign='top' bgcolor='#999999'><strong>Name</strong></td>
+      <td width='130' align='center' valign='top' bgcolor='#999999'><strong>Town Hall level</strong></td>
+	  <td width='133' align='center' valign='top' bgcolor='#999999'><strong>Last online</strong></td>
+	  <td width='42' align='center' valign='top' bgcolor='#999999'><strong>Status</strong></td>
+      <td width='136' align='center' valign='top' bgcolor='#999999'><strong>Server Permissions</strong></td>
     </tr>
 <?
-//Una consulta para obtener el resultado deseado
-$sql=mysql_query("SELECT * FROM player");
-    while($row = mysql_fetch_array($sql)){ 
-    $avatar = (json_decode($row["Avatar"], true));
-	$th = ($avatar['townhall_level']+1);
+include "config.php";
+$sql1=mysql_query("SELECT * FROM player");//cambiar nombre de la tabla de busqueda
+    while($row = mysql_fetch_array($sql1)){ 
+		$avatar = $row["Avatar"];
+		$avatarObj = json_decode($row["Avatar"], true);
+
+		$players1[] = array(
+		"avatarObj" => $avatarObj,
+		);
 	}
-$sql=mysql_query("SELECT * FROM player order by ".$avatar['townhall_level']." asc") or die ('Database name is not available!');
-//Ordenamos de forma ascendente.
-    while($row = mysql_fetch_array($sql)){ 
-    $avatar = (json_decode($row["Avatar"], true));
-    $playername = $avatar['avatar_name'];
-	$th = ($avatar['townhall_level']+1);
-	
+foreach($players1 as $player){
+	$th = $player['avatarObj']['townhall_level'] + 1;
+}
+$sql=mysql_query("SELECT * FROM player order by ".$th." asc") or die ('Database name is not available!');
+//cambiar nombre de la tabla de busqueda
+        while($row = mysql_fetch_array($sql)){ 
+		$playerID = $row['PlayerId'];
+		$AccountStatus = $row['AccountStatus'];
+		$AccountPrivileges = $row['AccountPrivileges'];
+		$LastUpdateTime = $row['LastUpdateTime'];
+		
+		$avatar = $row["Avatar"];
+		$avatarObj = json_decode($row["Avatar"], true);
+		
+		$players[] = array(
+			"PlayerId" => $row['PlayerId'],
+			"AccountStatus" => $row['AccountStatus'],
+			"AccountPrivileges" => $row['AccountPrivileges'],
+			"LastUpdateTime" => $row['LastUpdateTime'],
+			"avatar" => $avatar,
+			"avatarObj" => $avatarObj,
+		);
+	}
+foreach($players as $player){
+	$playername = $player['avatarObj']['avatar_name'];
+	$th = $player['avatarObj']['townhall_level'] + 1;
 echo "<tr bgcolor=''>
-      <td align='center' valign='top'>".$row["PlayerId"]."</td>
+      <td align='center' valign='top'>".$player["PlayerId"]."</td>
       <td align='center' valign='top'>".$playername."</td>
       <td align='center' valign='top'>".$th."</td>
-	  <td align='center' valign='top'>".$row["LastUpdateTime"]."</td>
-	  <td align='center' valign='top'><a href=\"#\" target=\"_blank\">Click Aquí</a></td>
+	  <td align='center' valign='top'>".$player["LastUpdateTime"]."</td>
+	  <td align='center' valign='top'>".$player["AccountStatus"]."</td>
+	  <td align='center' valign='top'>".$player["AccountPrivileges"]."</td>
     </tr>";
 	}
 ?>
