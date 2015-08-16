0f40086f403b758c05f87a199e11f49ce3263d06
JorgeMoralesMV <JorgeMoralesMV@users.noreply.github.com>
2015-08-15 23:31:46 -0500

    Ascending and descending fixed sentence and added the users trophies
    ascending and descending fixed sentence (line:25) and added the users trophies

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
-<table width='676' border='1' align='center' cellpadding='1' cellspacing='1'>
+<table width='745' border='1' align='center' cellpadding='1' cellspacing='1'>
     <tr>
-      <td width='106' align='center' valign='top' bgcolor='#999999'><strong>Player ID</strong></td>
-      <td width='96' align='center' valign='top' bgcolor='#999999'><strong>Name</strong></td>
-      <td width='130' align='center' valign='top' bgcolor='#999999'><strong>Town Hall level</strong></td>
-	  <td width='133' align='center' valign='top' bgcolor='#999999'><strong>Last online</strong></td>
-	  <td width='42' align='center' valign='top' bgcolor='#999999'><strong>Status</strong></td>
-      <td width='136' align='center' valign='top' bgcolor='#999999'><strong>Server Permissions</strong></td>
+      <td width='95' align='center' valign='top' bgcolor='#999999'><strong>Player ID</strong></td>
+      <td width='84' align='center' valign='top' bgcolor='#999999'><strong>Name</strong></td>
+      <td width='136' align='center' valign='top' bgcolor='#999999'><strong>Town Hall level</strong></td>
+	  <td width='108' align='center' valign='top' bgcolor='#999999'><strong>Last online</strong></td>
+	  <td width='50' align='center' valign='top' bgcolor='#999999'><strong>Status</strong></td>
+      <td width='135' align='center' valign='top' bgcolor='#999999'><strong>Server Permissions</strong></td>
+      <td width='99' align='center' valign='top' bgcolor='#999999'><strong>Trophies</strong></td>
     </tr>
 <?
 include "config.php";
@@ -15,13 +16,13 @@ $sql1=mysql_query("SELECT * FROM player");//cambiar nombre de la tabla de busque
 		$avatarObj = json_decode($row["Avatar"], true);
 
 		$players1[] = array(
-		"avatarObj" => $avatarObj,
+			"avatarObj" => $avatarObj,
 		);
 	}
 foreach($players1 as $player){
 	$th = $player['avatarObj']['townhall_level'] + 1;
 }
-$sql=mysql_query("SELECT * FROM player order by ".$player['avatarObj']['townhall_level']." asc") or die ('Database name is not available!');
+$sql=mysql_query("select * FROM player order by ".$player['avatarObj']['townhall_level']." asc") or die ('Database name is not available!');
 //cambiar nombre de la tabla de busqueda
         while($row = mysql_fetch_array($sql)){ 
 		$playerID = $row['PlayerId'];
@@ -44,6 +45,7 @@ $sql=mysql_query("SELECT * FROM player order by ".$player['avatarObj']['townhall
 foreach($players as $player){
 	$playername = $player['avatarObj']['avatar_name'];
 	$th = $player['avatarObj']['townhall_level'] + 1;
+	$sc = $player['avatarObj']['score'];
 echo "<tr bgcolor=''>
       <td align='center' valign='top'>".$player["PlayerId"]."</td>
       <td align='center' valign='top'>".$playername."</td>
@@ -51,7 +53,8 @@ echo "<tr bgcolor=''>
 	  <td align='center' valign='top'>".$player["LastUpdateTime"]."</td>
 	  <td align='center' valign='top'>".$player["AccountStatus"]."</td>
 	  <td align='center' valign='top'>".$player["AccountPrivileges"]."</td>
+	  <td align='center' valign='top'>".$sc."</td>
     </tr>";
 	}
 ?>
-</table> 
+</table>
