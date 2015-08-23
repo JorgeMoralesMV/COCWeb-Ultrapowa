fe183b673281e6e1e403afba18440707f22510db
JorgeMoralesMV <gemorales.va@gmail.com>
2015-08-23 17:26:42 -0500

    Small changes and fixes, inclusion of trophies with pictures
    Small changes and fixes, inclusion of trophies with pictures
    Thanks AsH.H6

--- index.php
+++ index.php
@@ -1,73 +1,26 @@
-<style type="text/css">
-<!--
-.red {
-	color: #800000;
-}
-.grey {
-	color: #000000;
-}
-.gold {
-	color: #BEC402;
-}
-.blue {
-	color: #0080FF;
-}
-.green {
-	color: #00FF40;
-}
-.themain {
-background: #d56900;
--moz-border-radius: 8px 8px 8px 8px;
-font-family:Georgia, "Times New Roman", Times, serif;
-}
-
-.topp1 {
-font-size:13px;
-background: #ff7e00;
-color: #ffcfcf;
-font-weight: bold;
--moz-border-radius: 8px 0 0 0;
-}
-
-.topp2 {
-font-size:13px;
-background: #ff7e00;
-color: #ffcfcf;
-font-weight: bold;
-}
-
-.topp3 {
-font-size:13px;
-background: #ff7e00;
-color: #ffcfcf;
-font-weight: bold;
-}
-.trhover {
-background: #FFFFFF;
-}
-
-.trhover:hover{background: #ffeab0;}
-.trhover:active{background: #ffeab0;}
-
--->
-</style>
+<html>
+<head>
+<link rel="stylesheet" href="style.css" />
+<title>Server Status</title>
+</head>
+<body>
 <div align="center"><img src="images/image.jpg" alt="" width="420" height="179" border="0"><br /></div><br />
-<table class="themain" align="center" cellpadding="2" cellspacing="0" width="10%"><tbody><tr><td>
-<table width='1054' class="themain" border='0' align='center' cellpadding='1' cellspacing='1'>
+<table class='themain' align="center" cellpadding='2' cellspacing='0' width='10%'><tbody><tr><td>
+<table width='1054' class='themain' border='0' align='center' cellpadding='1' cellspacing='1'>
     <tr>
-      <td width='43' align='center' valign='top' class="topp2"><strong>#</strong></td>
-      <td width='119' align='center' valign='top' class="topp2"><strong>Player ID</strong></td>
-      <td width='70' align='center' valign='top' class="topp2"><strong>Name</strong></td>
-      <td width='107' align='center' valign='top' class="topp2"><strong>Town Hall</strong></td>
-      <td width='67' align='center' valign='top' class="topp2"><strong>Level</strong></td>
-	  <td width='133' align='center' valign='top' class="topp2"><strong>Latest Update</strong></td>
-	  <td width='55' align='center' valign='top' class="topp2"><strong>Status</strong></td>
-      <td width='131' align='center' valign='top' class="topp2"><strong>Account Privileges</strong></td>
-      <td width='70' align='center' valign='top' class="topp2"><strong>Trophies</strong></td>
-      <td width='86' align='center' valign='top' class="topp2"><strong>Gems</strong></td>
-      <td width='95' align='center' valign='top' class="topp2"><strong>Experience</strong></td>
-    </tr>
-<?
+      <td width='43' align='center' valign='top' class='topp2'><strong>#</strong></td>
+      <td width='119' align='center' valign='top' class='topp2'><strong>Player ID</strong></td>
+      <td width='70' align='center' valign='top' class='topp2'><strong>Name</strong></td>
+      <td width='107' align='center' valign='top' class='topp2'><strong>Town Hall</strong></td>
+      <td width='67' align='center' valign='top' class='topp2'><strong>Level</strong></td>
+	  <td width='133' align='center' valign='top' class='topp2'><strong>Latest Update</strong></td>
+	  <td width='55' align='center' valign='top' class='topp2'><strong>Status</strong></td>
+      <td width='131' align='center' valign='top' class='topp2'><strong>Account Privileges</strong></td>
+      <td width='70' align='center' valign='top' class='topp2'><strong>Trophies</strong></td>
+      <td width='86' align='center' valign='top' class='topp2'><strong>Gems</strong></td>
+      <td width='95' align='center' valign='top' class='topp2'><strong>Experience</strong></td>
+   </tr>
+<?PHP
 include "config.php";
 $sql1=mysql_query("SELECT * FROM player");//cambiar nombre de la tabla de busqueda
     while($row = mysql_fetch_array($sql1)){ 
@@ -81,7 +34,7 @@ $sql1=mysql_query("SELECT * FROM player");//cambiar nombre de la tabla de busque
 foreach($players1 as $player){
 	$th = $player['avatarObj']['townhall_level'] + 1;
 }
-$sql=mysql_query("select * FROM player order by ".$player['avatarObj']['townhall_level']." desc") or die ('Database name is not available!');
+$sql=mysql_query("SELECT * FROM `player` ORDER BY ".$player['avatarObj']['townhall_level']." ASC");
 //cambiar nombre de la tabla de busqueda
         $i = 0;
         while($row = mysql_fetch_array($sql)){ 
@@ -110,47 +63,70 @@ foreach($players as $player){
 	$gems = $player['avatarObj']['current_gems'];
 	$exp = $player['avatarObj']['experience'];
 	$i = $i+1;
-if($th == 1){ $th ='<img src="images/townhall/'.$th.'.png" alt="1" width="30" height="30"><FONT SIZE=1>'.$th.'</font>';
+//townhall
+if($th == 1){ $th ='<img src="images/townhall/'.$th.'.png" alt="1" width="60" height="60"><FONT SIZE=1>'.$th.'</font>';
 }
-if($th == 2){ $th ='<img src="images/townhall/'.$th.'.png" alt="2" width="30" height="30"><FONT SIZE=1>'.$th.'</font>';
+if($th == 2){ $th ='<img src="images/townhall/'.$th.'.png" alt="2" width="60" height="60"><FONT SIZE=1>'.$th.'</font>';
 }
-if($th == 3){ $th ='<img src="images/townhall/'.$th.'.png" alt="3" width="30" height="30"><FONT SIZE=1>'.$th.'</font>';
+if($th == 3){ $th ='<img src="images/townhall/'.$th.'.png" alt="3" width="60" height="60"><FONT SIZE=1>'.$th.'</font>';
 }
-if($th == 4){ $th ='<img src="images/townhall/'.$th.'.png" alt="4" width="30" height="30"><FONT SIZE=1>'.$th.'</font>';
+if($th == 4){ $th ='<img src="images/townhall/'.$th.'.png" alt="4" width="60" height="60"><FONT SIZE=1>'.$th.'</font>';
 }
-if($th == 5){ $th ='<img src="images/townhall/'.$th.'.png" alt="5" width="30" height="30"><FONT SIZE=1>'.$th.'</font>';
+if($th == 5){ $th ='<img src="images/townhall/'.$th.'.png" alt="5" width="60" height="60"><FONT SIZE=1>'.$th.'</font>';
 }
-if($th == 6){ $th ='<img src="images/townhall/'.$th.'.png" alt="6" width="30" height="30"><FONT SIZE=1>'.$th.'</font>';
+if($th == 6){ $th ='<img src="images/townhall/'.$th.'.png" alt="6" width="60" height="60"><FONT SIZE=1>'.$th.'</font>';
 }
-if($th == 7){ $th ='<img src="images/townhall/'.$th.'.png" alt="7" width="30" height="30"><FONT SIZE=1>'.$th.'</font>';
+if($th == 7){ $th ='<img src="images/townhall/'.$th.'.png" alt="7" width="60" height="60"><FONT SIZE=1>'.$th.'</font>';
 }
-if($th == 8){ $th ='<img src="images/townhall/'.$th.'.png" alt="8" width="30" height="30"><FONT SIZE=1>'.$th.'</font>';
+if($th == 8){ $th ='<img src="images/townhall/'.$th.'.png" alt="8" width="60" height="60"><FONT SIZE=1>'.$th.'</font>';
 }
-if($th == 9){ $th ='<img src="images/townhall/'.$th.'.png" alt="9" width="30" height="30"><FONT SIZE=1>'.$th.'</font>';
+if($th == 9){ $th ='<img src="images/townhall/'.$th.'.png" alt="9" width="60" height="60"><FONT SIZE=1>'.$th.'</font>';
 }
-if($th == 10){ $th ='<img src="images/townhall/'.$th.'.png" alt="10" width="30" height="30"><FONT SIZE=1>'.$th.'</font>';
+if($th == 10){ $th ='<img src="images/townhall/'.$th.'.png" alt="10" width="60" height="60"><FONT SIZE=1>'.$th.'</font>';
 }
 //score
-$legend = 4000;
+$lt=array(0,400,500,600,800,1000,1200,1400,1600,1800,2000,2200,2400,2600,2800,3000,3200,3500,3800,4100,4400,4700,5000,9999);
+$lt2=array(399,499,599,799,999,1199,1399,1599,1799,1999,2199,2399,2599,2799,2999,3199,3499,3799,4099,4399,4699,4999,99999);
 
-if($sc  == 3600){ $sc ='<img src="images/champion.png" alt="10" width="22" height="19">1';
+$legend = count($lt);
+for ($x = 0; $x < $legend; $x++)
+{
+    if (($player ['avatarObj']['score'] >= $lt[$x]) && ($player ['avatarObj']['score'] < $lt2[$x]))
+    {
+        $y = $x;
+        $sx = '<img src="images/'.$y.'.png" alt="10" width="22" height="19"><br>';
+    }
 }
-if($sc  == 3700){ $sc ='<img src="images/champion.png" alt="10" width="22" height="19">2';
+//AccountPrivileges
+if($player["AccountPrivileges"] == 0){ $player["AccountPrivileges"] ='Player';
 }
-if($sc   >= $legend){ $sc ='<img src="images/crystal.png" alt="10" width="22" height="19">L';
+if($player["AccountPrivileges"] == 1){ $player["AccountPrivileges"] ='<font color="red"><b>Moderator</b></font>';
+}
+if($player["AccountPrivileges"] == 2){ $player["AccountPrivileges"] ='<font color="red"><b>High Moderator</b></font>';
+}
+if($player["AccountPrivileges"] == 3){ $player["AccountPrivileges"] ='<font color="red"><b>Undefined</b></font>';
+}
+if($player["AccountPrivileges"] == 4){ $player["AccountPrivileges"] ='<font color="red"><b>Administrator</b></font>';
+}
+if($player["AccountPrivileges"] == 5){ $player["AccountPrivileges"] ='<font color="red"><b>Owner</b></font>';
+}
+//AccountStatus
+if($player["AccountStatus"] == 0){ $player["AccountStatus"] ='Normal';
+}
+if($player["AccountStatus"] <> 0){ $player["AccountStatus"] ='<font color="red">Banned</font>';
 }
 echo "<tr class='trhover'>
-      <td align='center' valign='top'>".$i."</td>
-      <td align='center' valign='top'>".$player["PlayerId"]."</td>
-      <td align='center' valign='top'>".$playername."</td>
-      <td align='center' valign='top'>".$th."</td>
-	  <td align='center' valign='top'>".$ava_level."</td>
-	  <td align='center' valign='top'>".$player["LastUpdateTime"]."</td>
-	  <td align='center' valign='top'>".$player["AccountStatus"]."</td>
-	  <td align='center' valign='top'>".$player["AccountPrivileges"]."</td>
-	  <td align='center' valign='top'>".$sc."</td>
-	  <td align='center' valign='top'>".$gems."</td>
-	  <td align='center' valign='top'>".$exp."</td>
+      <td align='center' valign='center'>".$i."</td>
+      <td align='center' valign='center'>".$player["PlayerId"]."</td>
+      <td align='center' valign='center'><a href='profile.php?id=".$player["PlayerId"]."'>".$playername."</a></td>
+      <td align='center' valign='center'>".$th."</td>
+	  <td align='center' valign='center'>".$ava_level."</td>
+	  <td align='center' valign='center'>".$player["LastUpdateTime"]."</td>
+	  <td align='center' valign='center'>".$player["AccountStatus"]."</td>
+	  <td align='center' valign='center'>".$player["AccountPrivileges"]."</td>
+	  <td align='center' valign='center'>".$sx."".$sc."</td>
+	  <td align='center' valign='center'>".$gems."</td>
+	  <td align='center' valign='center'>".$exp."</td>
 
     </tr>";
 	}
@@ -199,12 +175,12 @@ echo "<tr>
 </tr>
 <tr>
 <td class='trhover' align='center'>".++$count."</td>
-<td class='trhover' align='center'>Total Players Online:</td>	<td class='trhover' align='center'>$online /<span style='color:#F00;'>100</span></td>
+<td class='trhover' align='center'>Total Players Online:</td>	<td class='trhover' align='center'>$online /<span style='color:#F00;'>$totalacc[0]</span></td>
 </tr>
 
 ";
 ?>
-
-
-
-</table></table>
+</table>
+</table>
+</body>
+</html>
