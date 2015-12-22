cbc5e21454911a404dc76a85048d92a1549fbb11
Jorge Morales <JorgeMoralesMV@users.noreply.github.com>
2015-12-22 02:02:57 -0600

    header of your page
    menu, etc...

---
+++ header.php
@@ -0,0 +1,15 @@
+<table class='themain' align="center" cellpadding='2' cellspacing='0' width='51%'><tbody><tr>
+  <td>
+<table class='themain' border='0' align='center' cellpadding='1' cellspacing='1' width='100%'>
+  <tr>
+    <td colspan="5" align='center' valign='top' class='topp2'>Menu</td>
+  </tr>
+  <tr class='trhover'>
+    <td align='center' width="17%"><a href='index.php'>Home</a></td>
+    <td align='center' width="16%"><a href='search.php'>Search</a></td>
+    <td align='center' width="22%"><a href='blacklist.php'>BlackList</a></td>
+    <td align='center' width="18%"><a href='statistics.php'>Statistics</a></td>
+    <td align='center' width="27%"><a href='gamemasters.php'>Game Masters</a></td>
+  </tr>
+</table>
+</table>
