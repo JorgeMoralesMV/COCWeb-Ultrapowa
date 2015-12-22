071da77383e754edd27fd36cb41216d65a17ea29
Jorge Morales <JorgeMoralesMV@users.noreply.github.com>
2015-12-22 02:01:08 -0600

    footer for your site
    copyrigth, etc...

---
+++ footer.php
@@ -0,0 +1,11 @@
+<table class='themain' align="center" cellpadding='2' cellspacing='0' width='48%'><tbody><tr>
+  <td>
+<table class='themain' border='0' align='center' cellpadding='1' cellspacing='1' width='100%'>
+  <tr>
+    <td align='center' valign='top' class='topp2'>COCWeb™ Version 1.0</td>
+  </tr>
+  <tr class='trhover'>
+    <td align='center' valign='center'>Copyright ©2014 - 2015. All rights reserved.<br /><?=$myservername;?> By J0RG325</td>
+    </tr>
+</table>
+</table>
