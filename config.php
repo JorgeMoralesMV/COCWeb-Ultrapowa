8e14145281a80b7b42b7172bb1a6620bc68f6381
Jorge Morales <JorgeMoralesMV@users.noreply.github.com>
2015-12-22 01:58:01 -0600

    New config data
    Name, ip an url of your site

--- config.php
+++ config.php
@@ -15,4 +15,14 @@ $db_selected = mysql_select_db('ucsdb', $db_link);
 if(!$db_selected){
 die('No se selecciono la BD: ' . mysql_error());
 }
+/////////////////////Configs Page Info////////////////////
+$myservername = "Clash of THRONES";
+$site = "localhost";
+
+$NameOfServer1 = "Clash Server 1";
+$ipserver1 = "189.150.142.52";
+
+$NameOfServer2 = "Clash Server 2";
+$ipserver2 = "189.150.142.52";
+
 ?>
